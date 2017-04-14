4472d99b128bfdb57709735bb60f71a7cf0807c5
karpovVS <kazankarpov@gmail.com>
2017-04-14 12:00:08 +0300

    Add recording to file

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ class Logger
   def self.say_something
     puts "fff"
   end
+  def self.log_something wat
+    f = File.open 'log.txt', 'a'
+    f.puts wat
+    f.close
+  end
 end
 Logger.say_something
-
+Logger.log_something 'Wazzup'
